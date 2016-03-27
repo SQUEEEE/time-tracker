c89d2f8b61a4c3fcf8acaff2b297e9f9f384bd70
Erika Hellquist <erika.hellquist@live.com>
2016-03-27 16:46:28 +0200

    working active links in menubar

--- app/js/controllers/menuCtrl.js
+++ app/js/controllers/menuCtrl.js
@@ -1,5 +1,11 @@
-timeTrackerApp.controller('MenuCtrl', function($scope, TimeTracker) {
+timeTrackerApp.controller('MenuCtrl', function($scope, $location, TimeTracker) {
 
-
-
+    $scope.isActive = function (viewLocation) { 
+        if (viewLocation === $location.path()) {
+        	return "active";
+        }
+        else {
+        	return "";
+        }
+    };
 });
