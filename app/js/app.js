4bea613a6fb92da928055a9c79b5ca0ae3fdc25e
Erika Hellquist <erika.hellquist@live.com>
2016-03-27 12:07:13 +0200

    node seems to work now

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,18 @@ timeTrackerApp.config(['$routeProvider',
       when('/add', {
         templateUrl: 'partials/add.html'
       }).
+      when('/statistics', {
+        templateUrl: 'partials/statistics.html'
+      }).
+      when('/calendar', {
+        templateUrl: 'partials/calendar.html'
+      }).
+      when('/settings', {
+        templateUrl: 'partials/settings.html'
+      }).
+      when('/home', {
+        templateUrl: 'partials/home.html'
+      }).
       // redirects all other url:s to '/add'
       otherwise({
         redirectTo: '/add'
