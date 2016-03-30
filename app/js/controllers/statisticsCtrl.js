0a855362c2c03c53de2f20d09895917f70b718e6
Erika Hellquist <erika.hellquist@live.com>
2016-03-30 12:50:40 +0200

    starting on statistics

--- app/js/controllers/statisticsCtrl.js
+++ app/js/controllers/statisticsCtrl.js
@@ -1,5 +1,8 @@
 timeTrackerApp.controller('StatisticsCtrl', function($scope, TimeTracker) {
 
+	$scope.testCategories = TimeTracker.getTestCategories();
+
+	$scope.totalSum = TimeTracker.calcTimeAllCategories();
 
 
 });
