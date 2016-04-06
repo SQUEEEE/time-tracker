091011dbd6b0dbf00376ff3f46860d15681474fd
Erika Hellquist <erika.hellquist@live.com>
2016-04-06 09:00:26 +0200

    logout in menu works

--- app/js/controllers/menuCtrl.js
+++ app/js/controllers/menuCtrl.js
@@ -1,4 +1,6 @@
-timeTrackerApp.controller('MenuCtrl', function($scope, $location, TimeTracker) {
+timeTrackerApp.controller('MenuCtrl', function($scope, $location, TimeTracker, Auth) {
+
+	$scope.auth = Auth;
 
     $scope.isActive = function (viewLocation) { 
         if (viewLocation === $location.path()) {
