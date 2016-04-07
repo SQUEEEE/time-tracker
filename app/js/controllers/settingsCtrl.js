5b0bf53e0680bef53af53975c91ae9f7f70a48ab
Mollie <kvack_kitan@hotmail.com>
2016-04-07 10:46:28 +0200

    removed trash from before

--- app/js/controllers/settingsCtrl.js
+++ app/js/controllers/settingsCtrl.js
@@ -4,14 +4,4 @@ timeTrackerApp.controller('SettingsCtrl', function($scope, TimeTracker) {
 	$scope.colorArray = TimeTracker.getTestColors();
 	$scope.calendarArray = TimeTracker.getTestCalendars();
 
-	$scope.categories = function(){
-		var returnValue = '<ul class="list-group">';
-		for (index in $scope.categoryArray){
- 			returnValue = returnValue + "<li class='list-group-item'>" +
- 			 $scope.categoryArray[index] + " " + $scope.colorArray[index] + "</li>";
-		}
-		returnValue = returnValue + '</ul>'; 
-		//return $sce.trustAsHtml(returnValue)
-	}
-
 });
