bb09c5d4017e532b592f0b1446cefa0abed29fd7
Erika Hellquist <erika.hellquist@live.com>
2016-04-07 11:33:30 +0200

    working on timer

--- app/js/controllers/addCtrl.js
+++ app/js/controllers/addCtrl.js
@@ -1,23 +1,50 @@
 timeTrackerApp.controller('AddCtrl', function($scope, TimeTracker, currentAuth) {
 
 	$scope.timerRunning = false;
-	var timeStarted = false;
+	$scope.timeStarted = false;
 
 	console.log(currentAuth);
 
 	$scope.user = currentAuth.google.displayName;
 
+	$scope.durationSet = false;
+	$scope.duration = 0;
+
+	$scope.setDuration = function() {
+
+		$scope.durationSet = true;
+		console.log($scope.hours);
+		console.log($scope.min);
+		console.log($scope.sec);
+		console.log($scope.millis);
+
+		time = new Date(2016,1,1, $scope.hours, $scope.min, $scope.sec, 0);
+		console.log(time);
+		console.log(time.toLocaleTimeString());
+
+		$scope.duration = time.toLocaleTimeString();
+
+	}
+
+	$scope.$on('timer-stopped', function (event, data){
+		$scope.hours = data.hours;
+		$scope.min = data.minutes;
+		$scope.sec = data.seconds;
+		$scope.millis = data.millis;
+        console.log('Timer Stopped - data = ', data);
+    });
+
 
 	$scope.startTimer = function() {
-		if (!($scope.timerRunning) && !timeStarted) {
+		if (!($scope.timerRunning) && !$scope.timeStarted) {
 			$scope.$broadcast('timer-start');
 			$scope.timerRunning = true;
-			timeStarted = true;
+			$scope.timeStarted = true;
 		}
-		else if (!($scope.timerRunning) && timeStarted) {
+		else if (!($scope.timerRunning) && $scope.timeStarted) {
 			$scope.$broadcast('timer-resume');
 			$scope.timerRunning = true;
-			timeStarted = true;
+			$scope.timeStarted = true;
 		}
 	}
 
@@ -29,6 +56,18 @@ timeTrackerApp.controller('AddCtrl', function($scope, TimeTracker, currentAuth) 
 	$scope.resetTimer = function() {
 		$scope.$broadcast('timer-reset');
 		$scope.timerRunning = false;
-		timeStarted = false;	
+		$scope.timeStarted = false;	
 	}
+
+	$scope.testCategories = TimeTracker.getTestCategories();
+
+	$scope.selectCat = function(selected) {
+        for (index in $scope.testCategories) {
+            if (selected == $scope.testCategories[index]) { 
+                console.log("val: ", selected);
+            }
+        }
+    };
+
+
 });
